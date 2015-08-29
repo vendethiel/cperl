e70bc78a0717405579f253083c1bd456973661cd
Reini Urban <rurban@cpanel.net>
2015-08-29 11:27:50 +0200

    native: downgrade padsv at run-time, type earlier
    type and upgrade a PADSV earlier in a my_attrs declaration already.
    Add missing native padsv cases to op_lvalue_flags to avoid lvalue croaks.
    
    Do not touch the PAD_SV, rather box the OP.
    Change helper to op_native_padsv_off and op_native_padsv_on (only off is
    used).
    
    Avoid inserting a box directly before a nextstate. The stack result is
    ignored there.

--- embed.h
+++ embed.h
@@ -1791,7 +1791,8 @@
 #    endif
 #  endif
 #  if defined(PERL_IN_OP_C) || defined(PERL_IN_PP_HOT_C)
-#define op_native_padsv(a)	Perl_op_native_padsv(aTHX_ a)
+#define op_native_padsv_off(a)	Perl_op_native_padsv_off(aTHX_ a)
+#define op_native_padsv_on(a)	Perl_op_native_padsv_on(aTHX_ a)
 #  endif
 #  if defined(PERL_IN_OP_C) || defined(PERL_IN_SV_C)
 #define report_redefined_cv(a,b,c)	Perl_report_redefined_cv(aTHX_ a,b,c)
--- proto.h
+++ proto.h
@@ -8691,9 +8691,14 @@ STATIC void	S_op_clear_gv(pTHX_ OP* o, PADOFFSET *ixp)
 #  endif
 #endif
 #if defined(PERL_IN_OP_C) || defined(PERL_IN_PP_HOT_C)
-PERL_CALLCONV void	Perl_op_native_padsv(pTHX_ OP* o)
+PERL_CALLCONV void	Perl_op_native_padsv_off(pTHX_ OP* o)
 			__attribute__nonnull__(pTHX_1);
-#define PERL_ARGS_ASSERT_OP_NATIVE_PADSV	\
+#define PERL_ARGS_ASSERT_OP_NATIVE_PADSV_OFF	\
+	assert(o)
+
+PERL_CALLCONV void	Perl_op_native_padsv_on(pTHX_ OP* o)
+			__attribute__nonnull__(pTHX_1);
+#define PERL_ARGS_ASSERT_OP_NATIVE_PADSV_ON	\
 	assert(o)
 
 #endif
