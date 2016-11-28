3b40353a50949c8dcb5a34f0b29e7979ec1ca5ad
Reini Urban <rurban@cpanel.net>
2016-11-28 17:04:52 +0100

    smallstring: c++ fixes
    error: arithmetic on a pointer to void

--- perl.c
+++ perl.c
@@ -929,7 +929,7 @@ perl_destruct(pTHXx)
 	    safesysfree(environ[i]);
 
 	/* Must use safesysfree() when working with environ. */
-	safesysfree(environ);		
+	safesysfree((char*)environ);
 
 	environ = PL_origenviron;
     }
